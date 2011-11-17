d24739757d2223e37df8082258a66aee7bf7df46
unknown <sesaadmin@.(none)>
2011-11-17 13:46:31 -0500

    Updated Splitter Frame

--- splitterframe.py
+++ splitterframe.py
@@ -5,7 +5,7 @@ import wx
 import numpy as np
 import os.path
 
-path = "C:/Documents and Settings/adlwashi/My Documents/Neutron Data/"
+path = "C:/Documents and Settings/sesaadmin/My Documents/Neutron Data/"
 
 def partition(l,n):
     length = len(l)
@@ -48,8 +48,10 @@ class SplitterFrame(wx.Frame):
 
         run = int(self.runnumber.GetValue())
 
-        m = XMLManifest(path+"%i/Manifest.xml"%run,0)
-        subruns = m.getRuns(path+str(run))
+        directory = path+"%i/"%run
+
+        m = XMLManifest(directory+"Manifest.xml",0)
+        subruns = m.getRuns(directory)
 
         cutoff = int(self.time.GetValue())
 
@@ -58,14 +60,30 @@ class SplitterFrame(wx.Frame):
         print "Save!"
 
         for s,i in zip(sets,range(len(sets))):
-            with open(path+"%i/Combined_%i_%i.pel"%(run,run,i),"wb") as outfile:
-                head = np.fromfile(s[0].findDetector.get("path"),dtype=np.int8,count=256)
+            tottime = 0
+            with open(directory+"Combined_%i_%i.pel"%(run,i),"wb") as outfile:
+                head = np.fromfile(directory+s[0].find("Detector").get("path"),dtype=np.int8,count=256)
                 head.tofile(outfile)
-                for run in s:
-                    with open(run.find("Detector").get("path"),"rb") as infile:
+                
+                mon = np.zeros((1001,),dtype=np.int32)
+                
+                for subrun in s:
+                    tottime += float(subrun.get("time"))
+                    with open(directory+subrun.find("Detector").get("path"),"rb") as infile:
                         infile.seek(256)
-                        temp = np.fromfile(intfile,count=-1)
+                        temp = np.fromfile(infile,count=-1)
                         temp.tofile(outfile)
+                    with open(directory+subrun.find("Monitor").get("path"),"rb") as infile:
+                        montemp = np.loadtxt(infile,dtype=np.int32,skiprows=3)
+                        montemp = np.resize(montemp,(1001,2))
+                        mon += montemp[:,1]
+            with open(directory+"Combined_%i_%i.txt"%(run,i),"w") as outmon:                
+                outmon.write("File Saved for Run Number "+str(run)+"_"+str(i)+".\n")
+                outmon.write("This run had %d counts " % np.sum(mon))
+                outmon.write("and lasted %d milliseconds\n" % (float(tottime)*1000))
+                outmon.write("User Name=Unkown, Proposal Number=Unknown\n")
+                for i in range(0,1000):
+                    outmon.write("%d\t%d\n"%(i+1,mon[i]))
 
 if __name__=="__main__":
     app = wx.PySimpleApp()
