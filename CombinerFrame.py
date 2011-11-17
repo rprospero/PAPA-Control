dc307e48b675880ac74c42efb72e633002d3d4d2
unknown <sesaadmin@.(none)>
2011-11-17 14:36:27 -0500

    Separated Combiner from CombinerFrame

--- CombinerFrame.py
+++ CombinerFrame.py
@@ -1,6 +1,7 @@
 """This module contains an application for combining runs files"""
 
 from XMLManifest import XMLManifest
+import Combiner
 #from XMLConfig import XMLConfig
 import wx
 import numpy as np
@@ -79,38 +80,9 @@ class CombinerFrame(wx.Frame):
         """Combine the manifests"""
         paths = self.listbox.GetItems()
 
-        currents = set([])#List of the configurations of the instrument
-        runsets = {}#Directionary of lists of run nodes, indexed by their instrument configuration
+        currents,runsets = Combiner.load(paths)
 
-        #Get the subruns organized by flipper current
-        for path in paths:
-            base = os.path.dirname(path)
-            print(base)
-            manifest = XMLManifest(path,0)
-            subruns = manifest.getRuns(base)
-            print(subruns)
-            for subrun in subruns:
-                triangles = sorted(subrun.findall('Triangle'),
-                                   key=lambda x:
-                                       x.get('number'))
-                current = (subrun.find('Flipper').text.strip(),
-                           subrun.find('GuideFields').text.strip(),
-                           subrun.find('PhaseCoil').text.strip(),
-                           subrun.find('SampleCoil').text.strip(),
-                           triangles[0].text.strip(),
-                           triangles[1].text.strip(),
-                           triangles[2].text.strip(),
-                           triangles[3].text.strip(),
-                           triangles[4].text.strip(),
-                           triangles[5].text.strip(),
-                           triangles[6].text.strip(),
-                           triangles[7].text.strip())
-                subrun.set("Base",base)
-                if current in currents:
-                    runsets[current].append(subrun)
-                else:
-                    currents.add(current)
-                    runsets[current]=[subrun]
+
         for current in currents:
             self.currentbox.Insert(str(current),0)
         self.runsets = runsets
@@ -128,57 +100,7 @@ class CombinerFrame(wx.Frame):
         index = self.currentbox.GetSelections()
         keys = [eval(self.currentbox.GetItems()[i]) for i in index]
 
-#        runlists = []
-#        for key in keys:
-#            runslists.join(self.runsets[key])
-
-        runs = [x for key in keys for x in self.runsets[key]]
-
-        print runs
-
-        mon = np.zeros((1001,),dtype=np.int32)
-        tottime = 0
-        totmon = 0
-        totdet = 0
-        with open(outfile,"wb") as pelfile:
-            base = runs[0].get("Base")
-            detpath=os.path.join(base,runs[0].find('Detector').get('path'))
-            with open(detpath,"rb") as infile:
-                header = np.fromfile(infile,dtype=np.int8,count=256)
-                header.tofile(pelfile)
-                del header
-            #load the individual subruns
-            for r in runs:
-                base = r.get("Base")
-                num = r.get('number')
-                time = float(r.get('time'))
-                moncount = int(r.find('Monitor').get('count'))
-                detcount = int(r.find('Detector').get('count'))
-                monpath = os.path.join(base,r.find('Monitor').get('path'))
-                detpath = os.path.join(base,r.find('Detector').get('path'))
-                if time <= 0 or moncount/time < minmon:
-                    print("Dropping subrun %s as the count rate is too low"%num)
-                    continue
-                tottime += time
-                totmon += moncount
-                totdet += detcount
-                with open(monpath,"r") as infile:
-                    montemp = np.loadtxt(infile,dtype=np.int32,skiprows=3)
-                    #Handle old data, which has one fewer rows
-                    montemp = np.resize(montemp,(1001,2))
-                    mon += montemp[:,1]
-                with open(detpath,"rb") as infile:
-                    infile.seek(256)
-                    dettemp = np.fromfile(infile,count=-1)
-                    dettemp.tofile(pelfile)
-                    del dettemp
-        with open(outfile+".txt","w") as stream:
-            stream.write("File Saved for Run Number %s.\n"%0)
-            stream.write("This run had %d counts " % np.sum(mon))
-            stream.write("and lasted %d milliseconds\n" % (float(tottime)*1000))
-            stream.write("User Name=Unkown, Proposal Number=Unknown\n")
-            for i in range(0,1000):
-                stream.write("%d\t%d\n"%(i+1,mon[i]))
+        Combiner.save(outfile,minmon,keys,self.runsets)
 
 if __name__=="__main__":
     app = wx.PySimpleApp()
