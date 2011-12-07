898f01a38d6b32026ff982a4fa9e38ef956dfe31
unknown <adlwashi@.ads.iu.edu>
2011-12-07 17:21:44 -0500

    Added command line control to Reduction.py

--- Reduction.py
+++ Reduction.py
@@ -3,7 +3,9 @@ from monfile import MonFile
 import matplotlib.pyplot as plt
 import Combiner
 import numpy as np
-import sys
+from optparse import OptionParser
+
+from optparse import OptionParser
 
 basedir = "C:/Documents and Settings/sesaadmin/My Documents/Neutron Data/"
 
@@ -73,14 +75,39 @@ def echodiff(run,names,split,outfile=None):
         plt.clf()
 
 if __name__=='__main__':
-    runs = [int(x) for x in sys.argv[1:]]
 
-#    export(runs,4,3,0.5)
-    start = 4.5
-    stop = 5.51
-    step = 0.05
-    names = [str(x) for x in list(np.arange(start,stop,step))]
-    #
-#    echoplot(runs[-1],names)
-    echodiff(runs[-1],names,187)
+    parser = OptionParser()
+
+    choices = {"flipper":0,"guides":1,"phase":2,"sample":3,"1":4,"2":5,"3":6,"4":7,"5":8,"6":9,"7":10,"8":11}
+
+    parser.add_option("-e","--export",action="store_true",help="Export into pel files")
+    parser.add_option("--sortby",action="store",type="choice",help="Which power supply is scanned",
+                      choices=choices.keys())
+    parser.add_option("--flip",action="store",type="choice",help="Which power supply runs the flipper",
+                      choices=choices.keys())
+    parser.add_option("--mon",action="store",type="float",help="Minimum monitor value",default=8)
+
+    parser.add_option("--start",action = "store",type="float", help="The starting current of the scan")
+    parser.add_option("--stop",action = "store", type="float", help="The ending current of the scan")
+    parser.add_option("--step",action = "store", type="float", help="The current step of the scan")
+
+    parser.add_option("--plot",action="store",type="choice",
+                      help="Where to make a simple plot or perform a height diff",
+                      choices=["plot","diff"],default="plot")
+
+    (options,runs) = parser.parse_args()
+
+    print options
+
+    runs = [int(x) for x in runs]
+
+    if options.export:
+        export(runs,choices[options.sortby],choices[options.flip],options.mon)
+
+    names = [str(x) for x in list(np.arange(options.start,options.stop,options.step))]
+
+    if options.plot=="plot":
+        echoplot(runs[-1],names)
+    elif options.plot=="diff":
+        echodiff(runs[-1],names,187)
 
