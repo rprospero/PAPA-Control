69e5111ec309b9ffdfc36444eca1bd8eae52a139
Adam Washington <adlwashi@indiana.edu>
2012-03-28 15:40:23 -0400

    added headerdumper for getting quick detector information from pel files

---
+++ headerdump.py
@@ -0,0 +1,16 @@
+from reader import PelFile
+from optparse import OptionParser
+
+basedir = "C:/Documents and Settings/sesaadmin/My Documents/Neutron Data/"
+
+if __name__=='__main__':
+
+    parser = OptionParser()
+
+    (options,runs) = parser.parse_args()
+
+    p = PelFile(basedir + "%04i/0001.pel"%int(runs[0]))
+    d = p.header._asdict()
+    for i in range(10):
+        print "x%dGain\t%d" % (i,d["x%dGain"%i])
+        print "y%dGain\t%d" % (i,d["y%dGain"%i])
