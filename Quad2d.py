72f26e3a7f7ad13cf97a44f1517204631ea24499
Adam Washington <adlwashi@indiana.edu>
2012-04-26 15:17:19 -0400

    Cleaned up long lines in Quad2d.py

--- Quad2d.py
+++ Quad2d.py
@@ -11,7 +11,8 @@ basedir = "C:/Documents and Settings/sesaadmin/My Documents/Neutron Data/"
 
 def raw(run,name,start=50,end=100):
     p = PelFile(basedir+"%04i/" % run + name+".pel")
-    mon = MonFile(basedir+"%04i/" % run + name+".pel.txt",False)
+    mon = MonFile(basedir+"%04i/" % run + name+
+                  ".pel.txt",False)
     val = np.sum(p.make3d()[:,:,start:end],axis=2)
     print val.shape
     spectrum_total = np.sum(mon.spec)
@@ -22,17 +23,37 @@ if __name__=='__main__':
 
     parser = OptionParser()
 
-    choices = {None:None,"flipper":0,"guides":1,"phase":2,"sample":3,"1":4,"2":5,"3":6,"4":7,"5":8,"6":9,"7":10,"8":11}
+    choices = {None:None,"flipper":0,"guides":1,
+               "phase":2,"sample":3,"1":4,"2":5,
+               "3":6,"4":7,"5":8,"6":9,"7":10,"8":11}
 
-    parser.add_option("--mon",action="store",type="float",help="Minimum monitor value",default=8)
-    parser.add_option("--vmin",action="store",type="float",help="Minimum efficiency value",default=0)
-    parser.add_option("--cutoff",action="store",type="float",help="Minimum count rate",default=1e-6)    
+    parser.add_option("--mon",action="store",
+                      type="float",
+                      help="Minimum monitor value",
+                      default=8)
+    parser.add_option("--vmin",action="store",
+                      type="float",
+                      help="Minimum efficiency value",
+                      default=0)
+    parser.add_option("--cutoff",action="store",
+                      type="float",
+                      help="Minimum count rate",
+                      default=1e-6)    
     parser.add_option("--plot",action="store_true")
-    parser.add_option("--save",action="store",type="string",help="File to save data")
-    parser.add_option("--start",action="store",type="int",help="Beginning wavelength in 1/10 Angstrom units (e.g. 57 = 5.7 Angstoms).  The default value is 50",default=50)
-    parser.add_option("--stop",action="store",type="int",help="Ending wavelength in 1/10 Angstrom units (e.g. 57 = 5.7 Angstoms).  The default value is 100",default=100)
-    parser.add_option("--display",action="store",type="choice",choices=["cryo","solenoid","instrument"],
-                      help="Whether to plot the efficiency of the cryo flipper, the solenoid"+
+    parser.add_option("--save",action="store",
+                      type="string",
+                      help="File to save data")
+    parser.add_option("--start",action="store",
+                      type="int",help="Beginning wavelength in 1/10 Angstrom units (e.g. 57 = 5.7 Angstoms).  The default value is 50",default=50)
+    parser.add_option("--stop",action="store",
+                      type="int",help="Ending wavelength in 1/10 Angstrom units (e.g. 57 = 5.7 Angstoms).  The default value is 100",default=100)
+    parser.add_option("--display",action="store",
+                      type="choice",
+                      choices=["cryo","solenoid",
+                               "instrument"],
+                      help="Whether to plot the " +
+                      "efficiency of the cryo flipper,"+
+                      "the solenoid"+
                       " flipper, or the instrument")
 
     (options,runs) = parser.parse_args()
@@ -44,10 +65,12 @@ if __name__=='__main__':
 
     w = raw(runs[-1],"w",start,stop)
 
-    f,df,f1,df1,papb,dpapb,n,dn = getf(w,
-                                       raw(runs[-1],"x",start,stop),
-                                       raw(runs[-1],"y",start,stop),
-                                       raw(runs[-1],"z",start,stop))
+    data= getf(w,
+               raw(runs[-1],"x",start,stop),
+               raw(runs[-1],"y",start,stop),
+               raw(runs[-1],"z",start,stop))
+
+    f,df,f1,df1,papb,dpapb,n,dn = data
 
     if options.plot:
         if options.display=="cryo":
@@ -66,12 +89,15 @@ if __name__=='__main__':
     if options.save:
         with open(options.save,"w") as outfile:
             outfile.write(
-                "wave\tcryo\tcryoerr\tsolenoid\tsolenoiderr\t"+
-                "instrument\tinstrumenterr\tintensity\tinternsityerr\n")
+                "wave\tcryo\tcryoerr\tsolenoid\t"+
+                "solenoiderr\tinstrument\t"+
+                "instrumenterr\tintensity\t"+
+                "internsityerr\n")
             for x in range(512):
                 for y in range(512):
                     outfile.write(
-                        "%i\t%i\t%e\t%e\t%e\t%e\t%e\t%e\t%e\t%e\n"%
+                        ("%i\t%i\t%e\t%e\t%e\t%e\t"+
+                         "%e\t%e\t%e\t%e\n")%
                         (x,y,f[x,y],df[x,y],
                          f1[x,y],df1[x,y]
                          papb[x,y],dpapb[x,y],
